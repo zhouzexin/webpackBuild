a6d1828454fe53ab56d9abf6a8ba034faff93283
zhujing <jing.zhu@2339.com>
2017-09-24 23:08:57 +0800

    更新配置文件

--- webpack.common.js
+++ webpack.common.js
@@ -31,6 +31,9 @@ module.exports = {
 			constants: path.resolve(__dirname, 'src/constants'),
 			containers: path.resolve(__dirname, 'src/containers'),
 			styles: path.resolve(__dirname, 'src/styles'),
+			routers: path.resolve(__dirname, 'src/routers'),
+			utils: path.resolve(__dirname, 'src/utils'),
+			configs: path.resolve(__dirname, 'src/configs')
 		},
 		extensions: ['.jsx', '.js', '.json', '.ts', '.less', '.scss', '.css']
 	},
